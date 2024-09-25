ba320d06ba8f8657386b4e56eca2cc75f70202f3
M <114847810+riraum@users.noreply.github.com>
2024-09-25 15:14:19 +0900

    Trying methods but not working yet

--- array-methods.js
+++ array-methods.js
@@ -12,3 +12,24 @@
 // camelize("-webkit-transition") == 'WebkitTransition';
 
 // P.S. Hint: use split to split the string into an array, transform it and join back.
+function camelize(str) {
+  // convert to array, removing -
+  let splitArray = str.split("-");
+  // map first character and capitalize it
+  let mappedArray = splitArray.map(function (item) {
+    item.charAt(0).toUpperCase;
+  });
+  // convert array back to string
+  mappedArray.join("");
+  return mappedArray;
+}
+camelize("background-color");
+
+// array.forEach(function(item) => {
+//   item.charAt(0).toUpperCase;
+//   return array;
+// });
+
+// newArray[i] = newArray[i].charAt(0).toUpperCase() + newArray[i].substr(1);
+
+//   array.map((item) => item.charAt(0).toUpperCase);
