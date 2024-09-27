d8e8bf869215802da5cf2dc4c461263a302056c9
M <114847810+riraum@users.noreply.github.com>
2024-09-27 15:07:36 +0900

    Function works, but creates new array, doesn't pass tests

--- array-methods-task3.js
+++ array-methods-task3.js
@@ -7,9 +7,13 @@
 
 // For instance:
 
-// let arr = [5, 3, 8, 1];
+let arr = [5, 3, 8, 1];
 
-// filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+filterRangeInPlace(arr, 1, 4);
+// removed the numbers except from 1 to 4
 
 // alert( arr ); // [3, 1]
-function filterRangeInPlace(arr, a, b) {}
+function filterRangeInPlace(arr, a, b) {
+  arr = arr.filter((item) => item >= a && item <= b);
+  console.log(arr);
+}
