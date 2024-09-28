0b0c11655bd4a05eb7d7dbd0c5764c9831e71c6d
M <114847810+riraum@users.noreply.github.com>
2024-09-28 11:07:36 +0900

    Shorter function

--- array-methods-task4.js
+++ array-methods-task4.js
@@ -7,9 +7,7 @@ let arr = [5, 2, 1, -10, 8];
 
 // alert( arr ); // 8, 5, 2, 1, -10
 function compare(a, b) {
-  if (a < b) return 1;
-  if (a == b) return 0;
-  if (a > b) return -1;
+  return b - a;
 }
 arr.sort(compare);
 console.log(arr);
