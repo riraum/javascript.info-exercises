895ca754cc481989cfc812614c0543a7e21fd902
M <114847810+riraum@users.noreply.github.com>
2024-09-28 15:49:47 +0900

    Solve exercise

--- array-methods-task7.js
+++ array-methods-task7.js
@@ -12,6 +12,13 @@ let mary = { name: "Mary", age: 28 };
 let users = [john, pete, mary];
 // console.log(users);
 
-let names = john.name + pete.name + mary.name;
+let names = [john.name, pete.name, mary.name].join(", ");
 
+// john.name(john);
+// function user(users) {
+//   for (i = 0; i < users.length; i++) {
+//     return users.name;
+//     console.log(users.name);
+//   }
+// };
 console.log(names); // John, Pete, Mary
