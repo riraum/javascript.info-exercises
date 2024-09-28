93903e4cedc27b03493e26797b99f83e40042d85
M <114847810+riraum@users.noreply.github.com>
2024-09-28 14:42:35 +0900

    Comment out variables in task 3

--- array-methods-task3.js
+++ array-methods-task3.js
@@ -7,9 +7,9 @@
 
 // For instance:
 
-let arr = [5, 3, 8, 1];
+// let arr = [5, 3, 8, 1];
 
-filterRangeInPlace(arr, 1, 4);
+// filterRangeInPlace(arr, 1, 4);
 // removed the numbers except from 1 to 4
 
 // alert( arr ); // [3, 1]
