13d7fcedb80b1deee712037055a87fe670f3b91b
M <114847810+riraum@users.noreply.github.com>
2024-09-28 11:11:27 +0900

    Use arrow function

--- array-methods-task4.js
+++ array-methods-task4.js
@@ -6,8 +6,5 @@ let arr = [5, 2, 1, -10, 8];
 // // ... your code to sort it in decreasing order
 
 // alert( arr ); // 8, 5, 2, 1, -10
-function compare(a, b) {
-  return b - a;
-}
-arr.sort(compare);
+arr.sort((a, b) => b - a);
 console.log(arr);
