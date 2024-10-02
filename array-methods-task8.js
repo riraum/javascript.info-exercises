2fbf7b782ab428e88a0b32e2b56f596413b34c90
M <114847810+riraum@users.noreply.github.com>
2024-10-02 20:55:04 +0900

    Full name is output, ID still missing

--- array-methods-task8.js
+++ array-methods-task8.js
@@ -16,19 +16,25 @@ console.log(users8);
 
 // Own code
 let usersMapped =
+  // Initialize another array of objects
+  // Take users8 array of objects, value of name and surname and put them in new variable fullName
+
   /* ... your code ... */
   //
-  // users.map((item) => item.name);
+  users8.map((newName, oldId) => ({
+    fullName: newName.name + newName.surname,
+    id: oldId.id,
+  }));
 
-  // /*
-  // usersMapped = [
-  //   { fullName: "Mike Smith", id: 1 },
-  //   { fullName: "Ryan Hunt", id: 2 },
-  //   { fullName: "Anna Key", id: 3 }
-  // ]
-  // */
-
-  alert(usersMapped[0].id); // 1
-alert(usersMapped[0].fullName); // Mike Smith
+// /*
+// usersMapped = [
+//   { fullName: "Mike Smith", id: 1 },
+//   { fullName: "Ryan Hunt", id: 2 },
+//   { fullName: "Anna Key", id: 3 }
+// ]
+// */
+console.log(usersMapped);
+console.log(usersMapped[0].id); // 1
+console.log(usersMapped[0].fullName); // Mike Smith
 
 // So, actually you need to map one array of objects to another. Try using => here. There’s a small catch.
