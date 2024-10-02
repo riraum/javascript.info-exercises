646e3e7eeba5dd3563151b2615f22812e4822537
M <114847810+riraum@users.noreply.github.com>
2024-10-02 16:08:26 +0900

    Actually use parameter in function

--- array-methods-task7.js
+++ array-methods-task7.js
@@ -10,6 +10,7 @@ let pete = { name: "Pete", age: 30 };
 let mary = { name: "Mary", age: 28 };
 
 let users = [john, pete, mary];
+// let userstest = [john, pete];
 
 // Own code
 // Create function, to run array through
@@ -17,7 +18,7 @@ function arrayOfNames(array) {
   // Initialize empty array
   let inFunctionNames = [];
   // loop over array of users
-  for (let user of users) {
+  for (let user of array) {
     // Push user.name to array of names
     inFunctionNames.push(user.name);
   }
@@ -26,8 +27,11 @@ function arrayOfNames(array) {
 }
 // Assign names to be the function arrayOfNames and run array of users through it
 let names = arrayOfNames(users);
+// let nametest = arrayOfNames(userstest);
+// arrayOfNames([eve, joanne, anna]);
 // log output to console
 // console.log(names); // John, Pete, Mary
+// console.log(nametest);
 // // check if result is an array
 // console.log(Array.isArray(names));
 // console.log(users.name);
