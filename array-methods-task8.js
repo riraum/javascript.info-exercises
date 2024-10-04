fdd8df380df38e3aecd9a3ffd91f27f834e029da
M <114847810+riraum@users.noreply.github.com>
2024-10-04 17:14:52 +0900

    First step of implementing code from pseudo code

--- array-methods-task8.js
+++ array-methods-task8.js
@@ -15,30 +15,38 @@ let users8 = [mike, ryan, anna];
 console.log(users8);
 
 // Own code
-let usersMapped =
-  // create a new empty object
+let usersMapped = {};
 
-  // for each user in the array of users
-  //     generate the fullName by concatenating the user name and surname
-  //     insert it in the object, using the user id as the key
+// let newObj = {};
 
-  // return the object
-  /* ... your code ... */
-  //
-  // users8.map((newName, oldId) => ({
-  //   fullName: newName.name + newName.surname,
-  //   id: oldId.id,
-  // }));
+for (let user in users8) {
+  usersMapped.fullName = users8.name + users8.surname;
+  console.log(users8.fullName);
+  usersMapped.id = users8.id;
+}
+console.log(usersMapped);
+// create a new empty object
 
-  // /*
-  // usersMapped = [
-  //   { fullName: "Mike Smith", id: 1 },
-  //   { fullName: "Ryan Hunt", id: 2 },
-  //   { fullName: "Anna Key", id: 3 }
-  // ]
-  // */
-  console.log(usersMapped);
-console.log(usersMapped[0].id); // 1
-console.log(usersMapped[0].fullName); // Mike Smith
+// for each user in the array of users
+//     generate the fullName by concatenating the user name and surname
+//     insert it in the object, using the user id as the key
+
+// return the object
+/* ... your code ... */
+//
+// users8.map((newName, oldId) => ({
+//   fullName: newName.name + newName.surname,
+//   id: oldId.id,
+// }));
+
+// /*
+// usersMapped = [
+//   { fullName: "Mike Smith", id: 1 },
+//   { fullName: "Ryan Hunt", id: 2 },
+//   { fullName: "Anna Key", id: 3 }
+// ]
+// */
+// console.log(usersMapped[0].id); // 1
+// console.log(usersMapped[0].fullName); // Mike Smith
 
 // So, actually you need to map one array of objects to another. Try using => here. There’s a small catch.
